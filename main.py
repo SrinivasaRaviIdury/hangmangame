04fe78e85cf71d527ad8586a74955c2489733f2e
SrinivasaRaviAhlad <42990095+raviaahlad@users.noreply.github.com>
2020-12-11 16:01:14 +0530

    main file in hangman game
    Credits Angela Yu
    Famous Hangman Game With Python Code

---
+++ main.py
@@ -0,0 +1,46 @@
+import random
+import hangman_art
+from hangman_words import word_list
+from replit import clear
+
+end_of_game = False
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+lives = 6
+#Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    if guess in display:
+        print(f"You've already guessed {guess}")
+    if guess in chosen_word:
+        #Check guessed letter
+        print(f"{guess} is in the word")
+        for position in range(word_length):
+            letter = chosen_word[position]
+            # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+            if letter == guess:
+                display[position] = letter
+    else:
+        print(f"{guess} not in word, You lose a life ")
+        lives-=1
+        if lives==0:
+            end_of_game = True
+            print("You Lose.")
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
+    
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+    print(hangman_art.stages[lives])
+    
